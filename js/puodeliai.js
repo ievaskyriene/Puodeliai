78c1923faebaef75fb641be859d3c96a51cf3dbc
ieva Skyriene <ieva.skyriene@gmail.com>
2020-03-15 15:04:24 +0200

    FInalfinal

--- js/puodeliai.js
+++ js/puodeliai.js
@@ -3,7 +3,7 @@
 
     let tower = document.querySelector(".tower");
     let innertower = document.querySelector(".innertower");
-    let cupcounter = 20;
+    let cupcounter = 100;
 
 function rowcount(x){
     let HTML = "";
